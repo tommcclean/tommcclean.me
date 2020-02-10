2af908745dac599d443f09dbc81caca2c1c3f868
Thomas William McClean <spam@tommcclean.me>
2020-02-10 19:36:35 +0000

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      tommcclean.me
+    </h1>
+    <h2 class="subtitle">
+      My personal website
+    </h2>
+  </div>
+</template>
