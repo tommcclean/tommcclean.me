892971774b0bfe08275316050916ed23d8edd327
Thomas William McClean <spam@tommcclean.me>
2023-04-22 23:47:14 +0100

    Remove instagram logo

--- components/sections/introduction.vue
+++ components/sections/introduction.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="c-intro u-fullHeight u-positionRelative u-flexContainer u-imageBackground">
     <utilities-overlay>
-      <sections-corner-badge
-        href="https://www.instagram.com/thomaswilliammcclean/"
-        icon="instagram" />
-
       <utilities-content-wrapper>
         <div class="inner">
           <h1 class="title">Thomas William McClean</h1>
